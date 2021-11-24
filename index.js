0207d658ab0ddfa0b749638144ca7ea0cecb1ec3
micahbala <micahkulien@gmail.com>
2021-11-24 15:52:32 +0100

    add code for cors

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ mongoose
   .then(console.log("Connected to MongoDB!"))
   .catch((err) => console.log("Error: ", err));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api", authRoute);
 app.use("/api/users/", usersRoute);
 app.use("/api/notes/", notesRoute);
