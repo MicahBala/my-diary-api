217cf1732855b86f1f524c5f5e7dc7444f5f2170
micahbala <micahkulien@gmail.com>
2021-11-24 08:28:38 +0100

    modify code

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose
   .then(console.log("Connected to MongoDB!"))
   .catch((err) => console.log("Error: ", err));
 
+app.use("/", (req, res) => res.status(200).send("Wlecome!"));
 app.use("/api", authRoute);
 app.use("/api/users/", usersRoute);
 app.use("/api/notes/", notesRoute);
